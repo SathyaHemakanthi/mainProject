1f91166912e7658d29d1b3ff370acee2dfeb5651
Sathya Hemakanthi <92479405+SathyaHemakanthi@users.noreply.github.com>
2023-10-26 16:27:44 +0530

    chart

--- igrowth/src/App.js
+++ igrowth/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "./Data";
+import LineChart from "../components/LineChart";
 
-function App() {
+Chart.register(CategoryScale);
+
+  export default function App() {
+    const [chartData, setChartData] = useState({
+      // ...chart data
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <LineChart chartData={chartData} />
     </div>
   );
-}
+  }
 
 export default App;
