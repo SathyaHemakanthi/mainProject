cae4b93ebb14529440d1acecd79cf0a8047ec212
Sathya Hemakanthi <92479405+SathyaHemakanthi@users.noreply.github.com>
2023-10-27 16:37:06 +0530

    development tracker
    nested map function using hardcode array for display development activities.

--- igrowth/src/App.js
+++ igrowth/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 import Graph from './graph.js'
-
+import Growth from './BabyGrowthTable.js'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route exat path="/" element={<Graph/>} />
+          <Route exat path="/dev" element={<Growth/>} />
         </Routes>
       </Router>
     </div>
