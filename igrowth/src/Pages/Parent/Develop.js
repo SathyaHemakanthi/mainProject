ac406a171d32ad563e8bd58ce824b9e70d611f34
Sathya Hemakanthi <92479405+SathyaHemakanthi@users.noreply.github.com>
2023-10-31 16:30:43 +0530

    Development activities status update

--- igrowth/src/Pages/Parent/Develop.js
+++ igrowth/src/Pages/Parent/Develop.js
@@ -1,52 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import "./develop.css";
 
 const Develop = () => {
   const [data, setData] = useState([]);
-  const [updatedData, setUpdatedData] = useState([]);
-  const [selectedActivity, setSelectedActivity] = useState(null);
 
   useEffect(() => {
+    // Fetch data from the API and update the state
     fetch("http://localhost:8081/development_activities")
       .then((res) => res.json())
-      .then((data) => {
-        // Initialize the updatedData state with the fetched data
-        setUpdatedData(data);
-        setData(data);
-      })
+      .then((data) => setData(data))
       .catch((err) => console.log(err));
   }, []);
 
-  const handleStatusChange = (activityId, status) => {
-    // Update the selected activity's status in the updatedData state
-    const updated = updatedData.map((d) =>
-      d.activity_id === activityId ? { ...d, activity_status: status } : d
-    );
-    setUpdatedData(updated);
-  };
-
-  const handleActivityClick = (activityId) => {
-    // Highlight the selected activity and set it as the selected activity
-    setSelectedActivity(activityId);
+  const handleRadioChange = (event, index) => {
+    const newData = [...data];
+    newData[index].activity_status = event.target.value;
+    setData(newData);
   };
 
   const handleSubmit = () => {
-    // Send the updated data to your server
-    fetch("http://localhost:8081/update_development_activity_status", {
+    // Send the updated data to the server
+    fetch("http://localhost:8081/update_development_activities", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(updatedData),
+      body: JSON.stringify(data),
     })
       .then((res) => res.json())
-      .then((data) => {
-        console.log("Development activity status updated successfully", data);
+      .then((response) => { 
+        console.log(response);
       })
-      .catch((err) => {
-        console.error("Error updating development activity status:", err);
-      });
+      .catch((err) => console.error(err));
   };
 
   return (
@@ -54,37 +39,29 @@ const Develop = () => {
       <table>
         <thead>
           <tr>
-            <th>I</th>
+            <th>ID</th>
             <th>Name</th>
             <th>Status</th>
           </tr>
         </thead>
         <tbody>
           {data.map((d, i) => (
-            <tr key={i} onClick={() => handleActivityClick(d.activity_id)}>
+            <tr key={i}>
               <td>{d.activity_id}</td>
               <td>{d.activity_name}</td>
               <td>
                 <input
                   type="radio"
-                  name={`status-${d.activity_id}`}
                   value="Yes"
-                  checked={
-                    d.activity_id === selectedActivity &&
-                    d.activity_status === "Yes"
-                  }
-                  onChange={() => handleStatusChange(d.activity_id, "Yes")}
+                  checked={d.activity_status === "Yes"}
+                  onChange={(e) => handleRadioChange(e, i)}
                 />
                 Yes
                 <input
                   type="radio"
-                  name={`status-${ d.activity_id}`}
                   value="No"
-                  checked={
-                    d.activity_id === selectedActivity &&
-                    d.activity_status === "No"
-                  }
-                  onChange={() => handleStatusChange(d.activity_id, "No")}
+                  checked={d.activity_status === "No"}
+                  onChange={(e) => handleRadioChange(e, i)}
                 />
                 No
               </td>
@@ -97,4 +74,4 @@ const Develop = () => {
   );
 };
 
-export default Develop;
+export default Develop; 
