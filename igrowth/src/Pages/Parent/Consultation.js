9dbcbc58c329a5fe04bcc901a43fb018353c5510
IsuruMJA99 <mjaisurumadusanka@gmail.com>
2023-10-31 16:26:27 +0530

    Final Consultation CSS

--- igrowth/src/Pages/Parent/Consultation.js
+++ igrowth/src/Pages/Parent/Consultation.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./consultation.css";
 import prof from "./images/doctor.jpg";
 
@@ -12,19 +12,18 @@ function Consultation() {
   }, []);
   return (
     <div>
-      
-          {data.map((d,i)=>(
-            <div class="card" key={i}>
-            <div class="card-body" >
-               <img className="doctor" src={prof} alt="doctor" />
-          <h4 class="card-title">{d.activity_name}</h4>
-          <p class="card-text">{d.activity_id}</p>
-          <p class="card-contact">{d.activity_name}</p>
-              </div>
-
-          
+      {data.map((d, i) => (
+        <div class="card" key={i}>
+          <div class="card-body">
+            <img className="doctor" src={prof} alt="doctor" />
+            <div className="right_side">
+              <h4 class="card-title">{d.activity_name}</h4>
+              <p class="card-text">{d.activity_id}</p>
+              <p class="card-contact">{d.activity_name}</p>
+            </div>
+          </div>
         </div>
-        ))}
+      ))}
     </div>
   );
 }
