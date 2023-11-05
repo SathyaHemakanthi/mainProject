70acd732af5fd0d22d07ecf8b2670f06ec8a321b
IsuruMJA99 <mjaisurumadusanka@gmail.com>
2023-11-05 21:37:05 +0530

    Add midwife pages

---
+++ igrowth/src/Pages/Midwife/Baby_Detail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Baby_Detail() {
+  return (
+    <div>Baby_Detail</div>
+  )
+}
+
+export default Baby_Detail
---
+++ igrowth/src/Pages/Midwife/Create_Account.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Create_Account() {
+  return (
+    <div>Create_Account</div>
+  )
+}
+
+export default Create_Account
