ee9d261b51086a54dbea6bb0ec738e3cbed89b58
IsuruMJA99 <mjaisurumadusanka@gmail.com>
2023-11-06 12:17:11 +0530

    Add Search Bar to Midwife Folder

--- igrowth/src/Pages/Midwife/Baby_Detail.js
+++ igrowth/src/Pages/Midwife/Baby_Detail.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import SearchBar from './SearchBar'
+import './baby_detail.css';
 
 function Baby_Detail() {
   return (
+    <div className='table-search'>
     <div>Baby_Detail</div>
+     <SearchBar />
+     </div>
   )
 }
 
--- igrowth/src/Pages/Midwife/SearchBar.js
+++ igrowth/src/Pages/Midwife/SearchBar.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './searchbar.css';
 
 function SearchBar() {
+  const [id, setId] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleIdChange = (e) => {
+    setId(e.target.value);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Implement the search logic here using the 'id' and 'username' state variables
+  };
+
   return (
-    <div>SearchBar</div>
-  )
+    <div>
+      <input
+        className="input"
+        type="text"
+        placeholder="Search by ID"
+        value={id}
+        onChange={handleIdChange}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="Search by Username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
