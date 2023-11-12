33886e5e5502f8eb19cdd8efdec92e0a1cc5dea7
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-11-12 22:26:55 +0530

    12/11

--- Backend/server.js
+++ Backend/server.js
@@ -24,48 +24,16 @@ app.get('/',(re,res)=>{
     return res.json("From backend side");
 })
 
-app.get("/distinct_months", (req, res) => {
-  const sql = "SELECT DISTINCT activity_month FROM development_activities";
-  db.query(sql, (err, data) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-    const months = data.map((row) => row.month);
-    return res.json(months);
-  });
-});
-
-app.get("/development_activities", (req, res) => {
-  const sql = "SELECT * FROM development_activities";
-  db.query(sql, (err, data) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-    return res.json(data);
-  });
-});
-
-app.post("/insert_development_activities", (req, res) => {
-  const data = req.body;
-
-  data.forEach((record) => {
-    record.child_id = 1;
-  });
-
-  for (const record of data) {
-    const sql = "INSERT INTO development_activities_for_babies (activity_id, child_id, status) VALUES (?, ?, ?)";
-    db.query(sql, [record.activity_id, record.child_id, record.status], (err) => {
-      if (err) {
-        return res.status(500).json(err);
-      }
-    });
-  }
-
-  res.json({ message: "Data inserted successfully" });
-});
+app.get('/development_activities',(req,res)=>{
+     const sql = "SELECT * from development_activities";
+     db.query(sql,(err, data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+     })
+})
 
 app.get('/consultation', (req, res) => {
-    const sql = 'SELECT * FROM consultation';
+    const sql = 'SELECT * FROM development_activities';
     db.query(sql,(err, data)=>{
         if(err) return res.json(err);
         return res.json(data);
@@ -140,6 +108,17 @@ app.post('/igrowth/news', upload.single('image'), (req, res) => {
   });
 });
 
+
+
+
+//consultation
+app.get('/consultation', (req, res) => {
+  const sql = 'SELECT * FROM consultation';
+  db.query(sql,(err, data)=>{
+      if(err) return res.json(err);
+      return res.json(data);
+    });
+  });
 
 
 app.listen(8081,()=>{
