329549b857f118a0f534ad0e102ed31c7e6fe941
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-11-14 12:32:22 +0530

    14/11

--- Backend/server.js
+++ Backend/server.js
@@ -32,16 +32,25 @@ app.get('/development_activities',(req,res)=>{
      })
 })
 
-app.get('/consultation', (req, res) => {
-    const sql = 'SELECT * FROM consultation';
-    db.query(sql,(err, data)=>{
-        if(err) return res.json(err);
-        return res.json(data);
-    });
+  app.post('/update_development_activities', async (req, res) => {
+    try {
+      const data = req.body;
+  
+      // Loop through the data and execute update queries for each record
+      for (const record of data) {
+        const sql = 'UPDATE development_activities SET activity_status = ? WHERE activity_id = ?';
+        await db.query(sql, [record.activity_status, record.activity_id]);
+      }
+  
+      res.json({ message: 'Data updated successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while updating data' });
+    }
   });
 
-  
 
+  // news get method
   app.get('/news', (req, res) => {
     const sql = 'SELECT * FROM news';
     db.query(sql,(err, data)=>{
