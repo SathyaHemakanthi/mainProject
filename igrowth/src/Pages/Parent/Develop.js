444a5d7c21552a5ff0a319f90ea6674a6104ef2a
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-11-14 11:29:42 +0530

    14/11

--- igrowth/src/Pages/Parent/Develop.js
+++ igrowth/src/Pages/Parent/Develop.js
@@ -1,5 +1,4 @@
-<<<<<<< SathyaHemakanththi
-=======
+
 import React, { useState, useEffect } from "react";
 import "./develop.css";
 
@@ -78,4 +77,3 @@ const Develop = () => {
 };
 
 export default Develop; 
->>>>>>> main
