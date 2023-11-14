d32022abacc21ddd58a1b7a500852363a8a9631b
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-11-14 12:23:07 +0530

    14/11
    merge last

--- igrowth/src/Pages/Parent/Health.js
+++ igrowth/src/Pages/Parent/Health.js
@@ -1,13 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './health.css';
+
+const babyHealthTips = {
+  newborn: [
+    "Breastfeed your baby exclusively for the first 6 months.",
+    "Ensure a safe and comfortable sleep environment for your baby.",
+    "Wash your hands before handling your baby.",
+    "Vaccinate your baby according to the recommended schedule.",
+    "Provide plenty of tummy time for your baby to strengthen their muscles."
+  ],
+  twoMonths: [
+    "Introduce tummy time for your baby to encourage head control.",
+    "Start tracking your baby's growth and development with your pediatrician.",
+    "Engage in interactive play to promote your baby's sensory development.",
+    "Continue to follow the recommended vaccination schedule.",
+    "Practice safe sleep habits for your baby."
+  ],
+  fourMonths: [
+    "Introduce your baby to age-appropriate toys and activities.",
+    "Begin the process of weaning if you are breastfeeding.",
+    "Monitor your baby's milestones and development.",
+    "Provide a variety of sensory experiences for your baby.",
+    "Maintain a consistent sleep schedule."
+  ]
+};
 
 function Health() {
   return (
-    <div>
-        <h1 className='con_1'>Health</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>Health Tips for Newborn Babies</h1>
+        <ul className="HealthTips">
+          {babyHealthTips.newborn.map((tip, index) => (
+            <li key={index}>{tip}</li>
+          ))}
+        </ul>
+
+        <h1>Health Tips for Two-Month-Old Babies</h1>
+        <ul className="HealthTips">
+          {babyHealthTips.twoMonths.map((tip, index) => (
+            <li key={index}>{tip}</li>
+          ))}
+        </ul>
+
+        <h1>Health Tips for Four-Month-Old Babies</h1>
+        <ul className="HealthTips">
+          {babyHealthTips.fourMonths.map((tip, index) => (
+            <li key={index}>{tip}</li>
+          ))}
+        </ul>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Health;
+export default Health;
