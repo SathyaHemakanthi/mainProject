bd7daa6896880aa6dc7b0746c852d4143c49ebf2
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-11-14 20:26:43 +0530

    main merge

--- igrowth/src/App.js
+++ igrowth/src/App.js
@@ -1,7 +1,26 @@
+import {ReactDOM} from "react";
+import { createBrowserRouter , RouterProvider, Route, Outlet} from "react-router-dom";
+import { ScheduleComponent } from "@syncfusion/ej2-react-schedule";
+import Navbar from "./component/Navbar";
+import Navbar2 from "./component/Navbar2";
+import Header from "./component/Header";
+import News from "./Pages/Parent/News";
+import Growth from "./Pages/Parent/Growth";
+import Develop from "./Pages/Parent/Develop";
+import Vaccination from "./Pages/Parent/Vaccination";
+import Health from "./Pages/Parent/Health";
+import Consultation from "./Pages/Parent/Consultation";
+import Calendar from "./Pages/Parent/Vaccination";
+import Upload from "./Pages/Admin/Upload.js";
+import Signup from "./Pages/Admin/Signup.js";
+
+import Write from "./Pages/Admin/Write.js";
 
 
-function App() {
-  return (
+
+
+const Layout1 = ()=>{
+  return(
     <div>
       <Header/>
       <Navbar/>
@@ -110,7 +129,6 @@ function App(){
       
         <RouterProvider router={router} />
       
-
     </div>
   );
 }
---
+++ igrowth/src/Pages/Midwife/Measure.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Measure() {
+  return (
+    <div>Measure</div>
+  )
+}
+
+export default Measure
