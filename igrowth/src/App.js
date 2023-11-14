e7d512c177b8dc8dd59276274199df51c53445c3
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-11-14 12:09:44 +0530

    14/11

--- igrowth/src/App.js
+++ igrowth/src/App.js
@@ -1,5 +1,21 @@
 import {ReactDOM} from "react";
 import { createBrowserRouter , RouterProvider, Route, Outlet} from "react-router-dom";
+import { ScheduleComponent } from "@syncfusion/ej2-react-schedule";
+import Navbar from "./component/Navbar";
+import Navbar2 from "./component/Navbar2";
+import Header from "./component/Header";
+import News from "./Pages/Parent/News";
+import Growth from "./Pages/Parent/Growth";
+import Develop from "./Pages/Parent/Develop";
+import Vaccination from "./Pages/Parent/Vaccination";
+import Health from "./Pages/Parent/Health";
+import Consultation from "./Pages/Parent/Consultation";
+import Calendar from "./Pages/Parent/Vaccination";
+import Upload from "./Pages/Admin/Upload.js";
+
+
+
+
 
 const Layout1 = ()=>{
   return(
@@ -7,7 +23,6 @@ const Layout1 = ()=>{
       <Header/>
       <Navbar/>
       <Outlet/>
-
       {/* <Footer/> */}
     </div>
   );
@@ -26,7 +41,7 @@ const Layout2 = ()=>{
 
 const router = createBrowserRouter([
   {
-
+    path:"/",
     element:<Layout1/>,
     children:[
       {
@@ -69,7 +84,36 @@ const router = createBrowserRouter([
     element:<Layout2/>,
     children:[
       {
+        path:"/midwife/news" ,
+        element:<News />,
+      },
+      {
+        path:"/midwife/growth",
+        element:<Growth />,
 
+      },
+
+    ]
+
+  },
+
+  {
+    path:"/admin",
+    element:<Layout2/>,
+    children:[
+     
+      {
+        path:"/admin/upload" ,
+        element:<Upload />,
+      },
+
+      
+
+    ]
+
+  },
+
+]);
 
 function App(){
   return(
