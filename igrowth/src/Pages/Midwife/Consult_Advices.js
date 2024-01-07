b702d65dda1fcbc38e5302dc8ec66e800efca54f
Sathya Hemakanthi <92479405+SathyaHemakanthi@users.noreply.github.com>
2024-01-07 10:50:08 +0530

    Delete igrowth directory

--- igrowth/src/Pages/Midwife/Consult_Advices.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Consult_Advices() {
-  return (
-    <div>Consult_Advices</div>
-  )
-}
-
-export default Consult_Advices
--- igrowth/src/Pages/Midwife/View_Development.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function View_Development() {
-  return (
-    <div>View_Development</div>
-  )
-}
-
-export default View_Development
